e415fe4f4b7b781a3a657a5e8a109dbd3eb95d6e
smrk2415 <smarakgartia2415@gmail.com>
2025-03-25 20:58:55 -0400

    add conversion operation examples in L03_conversionOperation.js

---
+++ L03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+// "" => false
+// "smrk" => true
