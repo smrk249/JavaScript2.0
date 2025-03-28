4231c482c78f561aa640429ed8582f5ccd759296
smrk2415 <smarakgartia2415@gmail.com>
2025-03-28 11:54:59 -0400

    add examples for conversion operations, comparisons, and string manipulations

--- L03_conversionOperation.js
+++ L03_conversionOperation.js
@@ -19,3 +19,35 @@ console.log(boolIsLoggedIn);
 
 // "" => false
 // "smrk" => true
+
+
+// ************************** Operations **************
+
+let value = 3
+let negValue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "smrk"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
